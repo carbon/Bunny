8142c01ae323d4ec7546bc7e8f2e201563f6341b
Jason Nelson <jason@carbonmade.com>
2019-05-28 09:42:24 -0700

    Add DeleteHostnameRequest

---
+++ src/BunnyCdn/Actions/DeleteHostnameRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BunnyCdn
+{
+    public sealed class DeleteHostnameRequest
+    {
+        public DeleteHostnameRequest(long pullZoneId, string hostname)
+        {
+            PullZoneId = pullZoneId;
+            Hostname = hostname ?? throw new ArgumentNullException(nameof(hostname));
+        }
+
+        public long PullZoneId { get; }
+
+        public string Hostname { get; }
+    }
+}
