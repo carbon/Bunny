65a6fe16b2cb8364c78413a130468c7cb8d31cb1
Jason Nelson <jason@carbonmade.com>
2020-03-14 20:15:55 -0500

    Add EdgeRuleTests

---
+++ src/BunnyCdn.Tests/EdgeRuleTests.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Text.Json;
+
+using Xunit;
+
+namespace BunnyCdn.Tests
+{
+    public class EdgeRuleTests
+    {
+        [Fact]
+        public void Serialize()
+        {
+            _ = new BunnyCdnClient("abc");
+
+            var rule = new EdgeRule
+            {
+                ActionType = EdgeRuleActionType.BlockRequest,
+                Description = "RULE NAME",
+                Triggers = new[]
+                {
+                    new EdgeRuleTrigger
+                    {
+                       Parameter1 = "abc",
+                       PatternMatches = new[] { "1", "2", "3" },
+                       PatternMatchingType = MatchType.Any,
+                       Type = EdgeRuleTriggerType.RequestUrl
+                    }
+                }
+            };
+
+            Assert.Equal(@"{
+  ""ActionType"": 4,
+  ""Enabled"": false,
+  ""Description"": ""RULE NAME"",
+  ""Triggers"": [
+    {
+      ""Parameter1"": ""abc"",
+      ""PatternMatches"": [
+        ""1"",
+        ""2"",
+        ""3""
+      ],
+      ""Type"": 0,
+      ""PatternMatchingType"": 0
+    }
+  ]
+}", JsonSerializer.Serialize(rule, new JsonSerializerOptions { WriteIndented = true, IgnoreNullValues = true }));
+        }
+
+    }
+}
