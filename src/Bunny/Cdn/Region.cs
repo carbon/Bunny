cc6e54551467b968bf6d6cb1c53a323b6f35ba4f
Jason Nelson <jason@carbonmade.com>
2023-09-27 14:26:31 -0700

    Add Region model

---
+++ src/Bunny/Cdn/Region.cs
@@ -0,0 +1,22 @@
+﻿#nullable disable
+
+namespace Bunny.Cdn;
+
+public sealed class Region
+{
+    public string ContinentCode { get; set; }
+
+    public string CountryCode { get; set; }
+
+    public long Id { get; set; }
+    
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal PricePerGigabyte { get; set; }
+
+    public string RegionCode { get; set; }
+}
